b91d55067386ce176becbd390e5ce1790266c603
hssahan <huseyinssahan@gmail.com>
2015-08-01 23:42:34 +0100

    pure html

---
+++ webPureHTML/scripts.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Serkan-PC on 26/07/2015.
+ */
+function userCreateVal() {
+    var e = document.getElementById("email").value;
+    var u = document.getElementById("username").value;
+    var p = document.getElementById("password").value;
+    var pa= document.getElementById("passwordagain").value;
+    if (e.length==0){
+        document.getElementById("Error").innerHTML = "Email missing";
+    }else if (u.length==0){
+        document.getElementById("Error").innerHTML = "Username missing";
+    }else if (p.length<5){
+        document.getElementById("Error").innerHTML = "Password not long enough";
+    }else if (p!=pa){
+        document.getElementById("Error").innerHTML = "Passwords don't match";
+    }
+    else{
+        return true;
+    }
+    return false;
+}
+function loginVal(){
+    var u = document.getElementById("username").value;
+    var p = document.getElementById("password").value;
+    if (u.length==0){
+        document.getElementById("Error").innerHTML = "Username missing";
+    }else if (p.length<5){
+        document.getElementById("Error").innerHTML = "Password not long enough";
+    }else{
+        return true;
+    }
+    return false;
+}
